df7d6afb6ba19af7ca2398b41c8c6648f50cf700
FarruxDEV <farruxbeknozimov@gmail.com>
2023-04-28 09:50:00 +0500

    add model to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
-    UsersModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
